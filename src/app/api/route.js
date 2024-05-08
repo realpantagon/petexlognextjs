fa008c31dfef8d83c0b0f63cd840776fb857c59e
AuJung <nuttapong6280@gmail.com>
2024-05-09 02:08:15 +0700

    chore: Add dependencies and API route for Line notification

---
+++ src/app/api/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+export async function POST(req) {
+  const token = "bQpzIpNyMwmu9yf5PaZny40gpRyFYTteESRDeypWpqp";
+  const { message } = await req.json();
+  console.log(message);
+
+  try {
+    const response = await axios("https://notify-api.line.me/api/notify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${token}`,
+      },
+      data: `message=${JSON.stringify(message)}`,
+    });
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.error("Failed to send Line notification:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,263 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+import { Select, MenuItem, TextField, Button } from "@mui/material";
+// import Record from "./Record";
+import axios from "axios";
+
+function Forminput() {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [rate, setRate] = useState("");
+  const [amount, setAmount] = useState("");
+  const [type, setType] = useState("Buying");
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [currencies, setCurrencies] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const fetchCurrenciesData = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.get(
+        "https://api.airtable.com/v0/appXvdgNSlqDP9QwS/Table%201",
+        {
+          headers: {
+            Authorization:
+              "Bearer patJrmzFDvT8Qncac.657ccc7a50caaebd1e4a3a390acca8e67d06047dd779d5726b602d4febe8e383",
+          },
+        }
+      );
+      const currencyData = response.data.records.map((record) => ({
+        label: record.fields.Currency,
+        rate: record.fields.Rate,
+      }));
+      setCurrencies(currencyData);
+
+      // Clear stored data after successful fetch
+      localStorage.removeItem("formData");
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+      setIsRefreshing(false); // Set isRefreshing to false after fetching data
+    }
+  };
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("formData");
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    } else {
+      setData([]);
+    }
+
+    fetchCurrenciesData();
+
+    // Fetch data every minute
+    const interval = setInterval(() => {
+      setIsRefreshing(true); // Set isRefreshing to true before fetching data
+      fetchCurrenciesData();
+    }, 60000); // 60000 milliseconds = 1 minute
+
+    return () => {
+      clearInterval(interval); // Clean up the interval on component unmount
+    };
+  }, []);
+
+  const handleOptionChange = (event) => {
+    const selectedCurrency = event.target.value;
+    const currencyInfo = currencies.find(
+      (currency) => currency.label === selectedCurrency
+    );
+    setSelectedOption(selectedCurrency);
+    if (currencyInfo) {
+      setRate(currencyInfo.rate);
+    } else {
+      setRate(""); // Clear rate if currency not found
+    }
+  };
+
+  const handleInput1Change = (event) => {
+    setRate(event.target.value);
+  };
+
+  const handleInput2Change = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const handleTypeChange = (event) => {
+    setType(event.target.value);
+  };
+
+  const handleAddClick = () => {
+    const timestamp = new Date().toLocaleString("en-US", { hour12: false });
+    const newData = {
+      time: timestamp,
+      currency: selectedOption,
+      rate,
+      amount,
+      type,
+      total: type === "Buying" ? rate * amount : -(rate * amount),
+    };
+    setData([...data, newData]);
+    setSelectedOption("");
+    setRate("");
+    setAmount("");
+
+    // Store the updated data in localStorage
+    localStorage.setItem("formData", JSON.stringify([...data, newData]));
+
+    // Send Line notification
+    const message = `${timestamp}\n ${selectedOption}\n ${rate}\n ${amount}\n ${type}\n ${newData.total.toFixed(
+      2
+    )} baht`;
+    console.log(message);
+    sendLineNotification(message)
+      .then(() => console.log("Line notification sent successfully"))
+      .catch((error) =>
+        console.error("Error sending Line notification:", error)
+      );
+  };
+
+  const sendLineNotification = async (message) => {
+    const resp = await axios.post("/api", { message });
+    if (!response.ok) {
+      throw new Error("Failed to send Line notification");
+    }
+  };
+
+  const handleClearClick = () => {
+    if (window.confirm("Are you sure you want to delete all data?")) {
+      setData([]);
+      localStorage.removeItem("formData");
+    }
+  };
+
+  const exportToGoogleSheet = () => {
+    // Convert data to CSV format
+    const csvData = [
+      ["Time", "Currency", "Rate", "Amount", "Type", "Total"].join(","),
+      ...data.map(
+        (entry) =>
+          `${entry.time},${entry.currency},${entry.rate},${entry.amount},${
+            entry.type
+          },${entry.total.toFixed(2)}`
+      ),
+    ].join("\n");
+
+    // Create a temporary anchor element to download the CSV file
+    const downloadLink = document.createElement("a");
+    const blob = new Blob([csvData], { type: "text/csv;charset=utf-8;" });
+    const url = URL.createObjectURL(blob);
+    downloadLink.href = url;
+    downloadLink.download = "data.csv";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="p-5">
+      <div className="text-3xl text-center">PETEX DATA</div>
+      {isRefreshing && <div>Refreshing data...</div>}
+      <div className="grid grid-cols-10 gap-4 m-4 p-4 rounded-lg">
+        <button
+          onClick={() => {
+            setIsRefreshing(true);
+            fetchCurrenciesData();
+          }}
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4 h-full"
+        >
+          Refresh
+        </button>
+        <Select
+          value={selectedOption}
+          onChange={handleOptionChange}
+          className="col-span-2 select"
+        >
+          <MenuItem value="">Select a Currency</MenuItem>
+          {currencies.map((currency) => (
+            <MenuItem key={currency.label} value={currency.label}>
+              {currency.label}
+            </MenuItem>
+          ))}
+        </Select>
+        <div className="col-span-2">
+          <TextField
+            id="rate"
+            type="text"
+            value={rate}
+            onChange={handleInput1Change}
+            label="Rate"
+            variant="outlined"
+            className="w-full"
+            inputProps={{ pattern: "^[0-9]*.?[0-9]*$" }} // Allow numbers and decimals
+            error={!/^[0-9]*\.?[0-9]*$/.test(rate)} // Check if input is a valid number
+            helperText={
+              !/^[0-9]*\.?[0-9]*$/.test(rate)
+                ? "Please enter a valid number"
+                : ""
+            }
+          />
         </div>
+        <div className="col-span-2">
+          <TextField
+            id="amount"
+            type="text"
+            value={amount}
+            onChange={handleInput2Change}
+            label="Amount"
+            className="w-full"
+            variant="outlined"
+            inputProps={{ pattern: "^[0-9]*.?[0-9]*$" }} // Allow numbers and decimals
+            error={!/^[0-9]*\.?[0-9]*$/.test(amount)} // Check if input is a valid number
+            helperText={
+              !/^[0-9]*\.?[0-9]*$/.test(amount)
+                ? "Please enter a valid number"
+                : ""
+            }
+          />
+        </div>
+        <Select
+          value={type}
+          onChange={handleTypeChange}
+          className="col-span-2 type"
+        >
+          <MenuItem value="Buying">Buying</MenuItem>
+          <MenuItem value="Selling">Selling</MenuItem>
+        </Select>
+        <button
+          onClick={handleAddClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+        >
+          ADD
+        </button>
+      </div>
+      {/* <Record data={data} /> */}
+      <div className="Summary">
+        <p className="total">Total : </p>
+        <p className="totaldata">
+          {data.reduce((acc, item) => acc + item.total, 0).toFixed(2)}{" "}
+        </p>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <button
+        onClick={handleClearClick}
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4"
+      >
+        DELETE
+      </button>
+      <button
+        onClick={exportToGoogleSheet}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4"
+      >
+        Export as CSV
+      </button>
+    </div>
   );
 }
+
+export default Forminput;
