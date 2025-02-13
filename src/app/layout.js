7586539fa1bb771f8d38af548624ce2929fbf756
realpantagon <pichayoothunchainao@gmail.com>
2025-02-13 21:47:58 +0700

    Remove unused components and update layout metadata; add favicon links

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,12 +4,16 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "PetexLog",
+  title: "Petex",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
